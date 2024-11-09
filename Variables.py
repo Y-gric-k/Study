3f1bc5b6c18b0ea71bd6dab22ede5ba924cd0b94
Y-gric-k <ygrick1213@gmail.com>
2024-11-09 11:14:43 +0700

    Базовые структуры данных.

---
+++ Variables.py
@@ -0,0 +1,4 @@
+home_work = 12
+hours = 1.5
+name_of_course = "Python"
+print("Курс: " + name_of_course + ". Всего занятий: " + str(home_work) + ". Затрачено времени: " + str(hours) + "часа." + " Среднее выполнение одного занятия - " + str(home_work / int(hours)) + " минут.")
