9b5dcc5d408edebf524ac337f56e1a7da32e3b9d
Y-gric-k <ygrick1213@gmail.com>
2024-11-09 10:43:23 +0700

    Базовые структуры данных.

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Igor"
+print("Name: " + name)
+age = 22
+print("Age: " + str(age))
+new_age = 23
+print("New Age: " + str(new_age))
+is_student = True
+print("Is Student: " + str(is_student))
