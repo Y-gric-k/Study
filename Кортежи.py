aa7bbc390712d7e2fc9656e8ee2cdbd44ea79c15
Y-gric-k <ygrick1213@gmail.com>
2024-11-22 10:36:49 +0700

    Базовые структуры данных.

---
+++ Кортежи.py
@@ -0,0 +1,8 @@
+immutable_var = 1,2,3,True,"Python"
+print(immutable_var)
+# immutable_var[0] = 100
+# print(immutable_var)             #tuple - относиться к неизменяемым типам данных
+mutable_list = [1,2,3,True,"Python"]
+print(mutable_list)
+mutable_list [0] = "Home"
+print(mutable_list)
