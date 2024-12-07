4e3e515a2c32f6e2667565808a34521d67c77954
Y-gric-k <ygrick1213@gmail.com>
2024-12-07 12:59:41 +0700

    Базовые структуры данных.

--- module_2_3.py
+++ module_2_3.py
@@ -1,9 +1,10 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-positive = []
 number = 0
 while number < len(my_list):
-    if my_list[number] < 0:
+    if my_list[number] >= 0:
+        if my_list[number] != 0:
+            print(my_list[number])
+        number = number + 1
+        continue
+    else:
         break
-    positive.append(my_list[number])
-    number += 1
-print(positive)
